25fa892757ba7211533b48873a61118cadd2abb4
pipado <pipado@pipado>
2012-06-09 21:05:23 +0000

    first commit

---
+++ forms.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Mar 20, 2011
+
+@author: Mourad Mourafiq
+
+@license: closed application, My_licence, http://www.binpress.com/license/view/l/6f5700aefd2f24dd0a21d509ebd8cdf8
+
+@copyright: Copyright © 2011
+
+other contributers:
+'''
+
+from django import forms
+from django.utils.translation import ugettext, ugettext_lazy as _
+from messages.fields import *
+from permissions.models import *
+
+class PermissionForm(forms.Form):
+    """
+    A form to create a permission object
+    """
+    visibility = forms.ChoiceField(widget=forms.Select(), choices=VISIBILITY,
+                                       initial="1",) 
+    can_read = CommaSeparatedUserField(label=_(u"can_read"), required=False)
+    protection = forms.ChoiceField(widget=forms.Select(), choices=PROTECTION,
+                                       initial="3",)
+    can_write = CommaSeparatedUserField(label=_(u"can_write"), required=False)    
+    error_css_class = "error"
+    
+    def __init__(self, *args, **kwargs):
+        super(PermissionForm, self).__init__(*args, **kwargs)
+        self.fields['visibility'].widget.attrs['class'] = 'visibility_check'        
+        self.fields['can_read'].widget.attrs['class'] = 'visibility'                                
+        self.fields['protection'].widget.attrs['class'] = 'protection_check'        
+        self.fields['can_write'].widget.attrs['class'] = 'protection' 
+        
+    def clean_can_write(self):
+        """
+        Check that if cal_protection is set to semi-protected, the user should fill
+        the can_write
+        """
+        if self.cleaned_data['protection'] == "2":
+            if not self.cleaned_data['can_write']:
+                raise forms.ValidationError(_("You should indicate people who could write on this article."))
+            return self.cleaned_data['can_write']                                                                             
+    
+    def clean_can_read(self):
+        """
+        Check that if cal_visibility is set to semi-public, the user should fill
+        the can_read
+        """
+        if self.cleaned_data['visibility'] == "2":
+            if not self.cleaned_data['can_read']:
+                raise forms.ValidationError(_("You should indicate people who could read this article."))
+            return self.cleaned_data['can_read']
+    
+    def save(self, slug, request_user):        
+        permissions = Permission(permission_name='%s_perms' % slug,
+                                             visibility=self.cleaned_data['visibility'],
+                                             protection=self.cleaned_data['protection'])
+        permissions.save(commit=False)          
+        permissions.can_write.add(request_user)
+        permissions.can_read.add(request_user)
+        if self.cleaned_data['protection'] == "2":
+            for user in self.cleaned_data['can_write']:                    
+                permissions.can_write.add(user)
+        if self.cleaned_data['visibility'] == "2":
+            for user in self.cleaned_data['can_read']:                    
+                permissions.can_read.add(user)
+        permissions.save(commit=True)
+        return permissions
+                    
---
+++ models.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Mar 20, 2011
+
+@author: Mourad Mourafiq
+
+@license: closed application, My_licence, http://www.binpress.com/license/view/l/6f5700aefd2f24dd0a21d509ebd8cdf8
+
+@copyright: Copyright © 2011
+
+other contributers:
+'''
+from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib.contenttypes.models import ContentType
+
+PROTECTION = (
+    ("1", _("Open")),
+    ("2", _("Custom")),
+    ("3", _("Protected")),
+    )
+
+VISIBILITY = (
+    ("1", _("Public")),
+    ("2", _("Custom")),
+    ("3", _("Private")),
+    )
+
+class Permission(models.Model):
+    permission_name = models.CharField(max_length=255, verbose_name=_('Permission name'))
+    can_write = models.ManyToManyField(User, blank=True, null=True, related_name='write',
+                                       help_text=_('Select none to grant anonymous access.'))
+    can_read = models.ManyToManyField(User, blank=True, null=True, related_name='read',
+                                       help_text=_('Select none to grant anonymous access.'))
+    protection = models.CharField(max_length=1, verbose_name=_('Protection'),
+                                  choices=PROTECTION)
+    visibility = models.CharField(max_length=1, verbose_name=_('Visibility'),
+                                  choices=VISIBILITY)    
+    def __unicode__(self):
+        return self.permission_name
+    
+    
+    class Meta:
+        verbose_name = _('permission')
+        verbose_name_plural = _('permissions')        
+        
+    def can_read_obj(self, user):        
+        """ Check read permissions and return True/False."""
+        if self.visibility == "1":
+            return True
+        else:   
+            perms = self.can_read.all()
+            return perms.count() == 0 or perms.filter(pk__exact=user.id).count() > 0
+
+    def can_write_obj(self, user):
+        """ Check write permissions and return True/False."""        
+        if self.can_read_obj(user):
+            if self.protection == "1":
+                return True
+            else:    
+                perms = self.can_write.all()
+                return perms.count() == 0 or perms.filter(pk__exact=user.id).count() > 0
+            
+    def edit_can_write_obj(self, can_write):
+        """ change permissions to users on the current cal """
+        if self.protection == "2":
+                old_can_write = []
+                can_write_l = self.can_write.all()
+                for i in can_write_l:
+                    old_can_write.append(i) 
+                for user in can_write: 
+                    if user in old_can_write:
+                        old_can_write.remove(user)
+                    else:
+                        self.can_write.add(user)
+                    
+                for user in old_can_write:                
+                    self.can_write.remove(user)            
+                 
+    def edit_can_read_obj(self, can_read):
+        """ change permissions to users on the current cal """
+        if self.visibility == "2":
+                old_can_read = []            
+                can_read_l = self.can_read.all()            
+                for i in can_read_l:
+                    old_can_read.append(i)                
+                for user in can_read:         
+                    if user in old_can_read:          
+                        old_can_read.remove(user)
+                    else:                                            
+                        self.can_read.add(user)
+                for user in old_can_read:
+                    self.can_read.remove(user)                
+       
+            
+            
